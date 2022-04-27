3d20b7993ea4dcbfaee4ba560e96f9a505221046
curellich <Curellich@gmail.com>
2022-04-27 12:37:54 -0300

    "Solucionando problemas de deploy en gh-pages"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {Routes, Route, BrowserRouter, HashRouter} from "react-router-dom";
+import {Routes, Route, HashRouter} from "react-router-dom";
 import ComShowBlogs from "./blog/ShowBlogs";
 import CompCreateBlog from "./blog/CreateBlogs";
 import CompEditBlog from "./blog/EditBlogs";
@@ -8,8 +8,6 @@ import CompEditBlog from "./blog/EditBlogs";
 function App() {
     return (
         <div className="App">
-
-
             <HashRouter>
                 <Routes>
                     <Route path='/' element={<ComShowBlogs/>}/>
